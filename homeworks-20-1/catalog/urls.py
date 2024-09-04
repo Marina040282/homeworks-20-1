31ed4c2123d394751e07fa48bf3d608d6d9b2d52
Marina040282 <119851129+Marina040282@users.noreply.github.com>
2024-09-04 20:51:34 +0300

    Доработка по замечаниям

--- homeworks-20-1/catalog/urls.py
+++ homeworks-20-1/catalog/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
-from catalog.views import index, contacts
-from catalog.apps import CatalogConfig
+from views import IndexListView, contacts
+from apps import CatalogConfig
 
 app_name = CatalogConfig.name
 urlpatterns = [
                   path('', IndexListView.as_view(), name='index'),  # вывод главной страницы
-                  path('contacts/', cache_page(60)(UserDetailView.as_view()), name='contact'),
+                  path('contacts', contacts, name='contact'),
 
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  # Это добавляется один раз на проект
