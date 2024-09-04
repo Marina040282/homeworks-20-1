ca3e38fd856c5abb67b4a19b713636918f45c932
Marina040282 <119851129+Marina040282@users.noreply.github.com>
2024-09-04 20:49:38 +0300

    Update views.py
    Доработка по замечаниям

--- homeworks-20-1/catalog/views.py
+++ homeworks-20-1/catalog/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render
-
-
+from django.views.generic import ListView
+from models import Product
 # Create your views here.
 
 
-def index(request, *args, **kwargs):
-    return render(request, 'catalog/app_name/index.html')
+#def index(request, *args, **kwargs):
+#    return render(request, 'catalog/app_name/index.html')
+class IndexListView(ListView):
+    model = Product
+    template_name = 'catalog/index.html'
+    context_object_name = 'products'
+    paginate_by = 6
+
+    def get_queryset(self):
+        # get_queryset - запрос на выборку данных из БД
+        queryset = super().get_queryset()
+        return queryset
+
 
 
 def contacts(request, *args, **kwargs):
