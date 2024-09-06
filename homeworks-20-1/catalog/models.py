d1f864bd9be13be8e8d270e5c77ec95c8cb1cb2d
Marina040282 <119851129+Marina040282@users.noreply.github.com>
2024-09-06 12:58:19 +0300

    Доработка по замечаниям 3

--- homeworks-20-1/catalog/models.py
+++ homeworks-20-1/catalog/models.py
@@ -25,7 +25,7 @@ class Category(models.Model):
 class Product(models.Model):
     product_name = models.CharField(max_length=100, verbose_name='наименование')
     description_product = models.TextField(verbose_name='описание')
-    picture_product = models.ImageField(upload_to='blog/', null=True, blank=True, verbose_name='изображение')
+    picture_product = models.ImageField(upload_to='images/', null=True, blank=True, verbose_name='изображение')
     category_name = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='категория')
     price_product = models.DecimalField(max_digits=15, decimal_places=2, verbose_name='цена за покупку')
     date_of_creation_product = models.DateTimeField(auto_now_add=True, verbose_name='дата создания')
